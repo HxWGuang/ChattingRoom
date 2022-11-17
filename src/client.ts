0cbed43f5f0281b7a503bf99d79f056be5be068b
HxGuang <sherman.xu@qq.com>
2022-11-17 22:04:10 +0800

    优化客户端获取用户名与连接服务器的顺序逻辑

--- src/client.ts
+++ src/client.ts
@@ -1,31 +1,43 @@
 import * as net from 'node:net';
 import * as readline from 'node:readline';
 import {stdin as input, stdout as output} from 'node:process';
-import {clientInfo, socket} from "./entity/clientInfo";
-import {serverInfo} from "./entity/serverInfo";
+import {clientInfo, socket, serverInfo, eCommandType} from "./entity/defineType";
 
 const rl = readline.createInterface({input, output});
-
-let name: string;
 
 let client = new net.Socket();
 client.setEncoding('utf-8');
 
-console.info('请输入用户名:');
-rl.on('line', (input) => {
-    if (input.length <= 0) {
-        console.error("用户名不能为空！请重新输入!");
-    } else {
-        name = input;
-        rl.close();
+// console.info('请输入用户名:');
+// rl.on('line', (input) => {
+//     if (input.length <= 0) {
+//         console.error("用户名不能为空！请重新输入!");
+//     } else {
+//         name = input;
+//         rl.close();
+//
+//         client.connect(serverInfo.port, serverInfo.host, () => {
+//             console.info(`已连接到${client.remoteAddress}:${client.remotePort}`);
+//             client.write(`${eCommandType.login} ${name}`);
+//         });
+//     }
+// });
 
-        client.connect(serverInfo.port, serverInfo.host, () => {
-            console.info(`已连接到${client.remoteAddress}:${client.remotePort}`);
-            client.write(`login_name ${name}`);
-        });
-    }
+const waitUsername = new Promise((resolve) => {
+    rl.question('请输入用户名:\n', answer => {
+        resolve(answer);
+    });
 });
 
-client.on('data', (data) => {
-    console.info(`${data}`);
+waitUsername.then(username => {
+    client.connect(serverInfo.port, serverInfo.host, () => {
+        console.info(`已连接到${client.remoteAddress}:${client.remotePort}`);
+        client.write(`${eCommandType.login} ${username}`);
+    });
+});
+
+rl.on('line', (input) => {
+    client.on('data', (input) => {
+        client.write(input);
+    });
 });
