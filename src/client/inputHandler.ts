1bede0253fcc9299e322af7c6489a2be8d4da07b
HxGuang <sherman.xu@qq.com>
2022-12-04 15:30:06 +0800

    删除socket属性，修改函数调用方式

--- src/client/inputHandler.ts
+++ src/client/inputHandler.ts
@@ -1,8 +1,6 @@
-import * as net from "net";
 import {eCommandType} from "../share/utils/attTypeDefine";
 
 export class inputHandler {
-    private socket: net.Socket | undefined;
     private actions = new Map<eCommandType, Function>();
 
     register(type: eCommandType, callback: Function) {
@@ -12,7 +10,7 @@ export class inputHandler {
     execute(cmd: eCommandType, args:string[]) {
         let func = this.actions.get(cmd);
         if (func) {
-            func.apply(this.socket, args);
+            func(...args);
         } else {
             console.error(`未找到cmd: ${cmd}`);
         }
